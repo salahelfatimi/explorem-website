dde3d5b654a4743c68c72490de0610d95c38a706
salahelfatimi <salahfatimi16@gmail.com>
2024-03-12 00:06:50 +0000

    add

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,16 +1,22 @@
-import Navbar from './navbar'
-import Footer from './footer'
-import Head from 'next/head'
- 
+import Navbar from "./navbar";
+import Footer from "./footer";
+import Head from "next/head";
+
 export default function Layout({ children }) {
   return (
     <>
       <Head>
         <title>Explorem</title>
+        <link
+          rel="icon"
+          href="/image/aboutUS/logoHand.png"
+          type="image/png"
+          sizes="<generated>"
+        />
       </Head>
       <Navbar />
-      <main className='py-24 mb-auto'>{children}</main>
+      <main className="py-24 mb-auto">{children}</main>
       <Footer />
     </>
-  )
+  );
 }
