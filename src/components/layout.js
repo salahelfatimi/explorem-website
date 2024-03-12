90d4234724b61cbe30fed507b1400a4568e75872
salahelfatimi <salahfatimi16@gmail.com>
2024-03-12 00:00:34 +0000

    add ico

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,13 @@
 import Navbar from './navbar'
 import Footer from './footer'
+import Head from 'next/head'
  
 export default function Layout({ children }) {
   return (
     <>
+      <Head>
+        <title>Explorem</title>
+      </Head>
       <Navbar />
       <main className='py-24 mb-auto'>{children}</main>
       <Footer />
