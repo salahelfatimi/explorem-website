caf84ff1cb43c90ec1e3f281b568f4656b7d9583
salahelfatimi <salahfatimi16@gmail.com>
2024-03-13 23:09:55 +0000

    add

--- src/pages/components/infiniteHorizontalScroll.jsx
+++ src/pages/components/infiniteHorizontalScroll.jsx
@@ -5,26 +5,26 @@ const InfiniteHorizontalScroll = () => {
   return (
     <div className="flex overflow-hidden space-x-16 group ">
         <div className="flex space-x-16 animate-loop-scroll ">
-            <img loading="lazy" src="/image/aboutUs/slideLeft/image1.jpg" className="max-w-none rounded-xl h-60 w-auto" alt="explorem" />
-            <img loading="lazy" src="/image/aboutUs/slideLeft/image2.jpg" className="max-w-none rounded-xl h-60 w-auto" alt="explorem" />
-            <img loading="lazy" src="/image/aboutUs/slideLeft/image3.jpg" className="max-w-none rounded-xl h-60 w-auto" alt="explorem" />
-            <img loading="lazy" src="/image/aboutUs/slideLeft/image4.jpg" className="max-w-none rounded-xl h-60 w-auto" alt="explorem" />
-            <img loading="lazy" src="/image/aboutUs/slideLeft/image5.jpg" className="max-w-none rounded-xl h-60 w-auto" alt="explorem" />
-            <img loading="lazy" src="/image/aboutUs/slideLeft/image6.jpg" className="max-w-none rounded-xl h-60 w-auto" alt="explorem" />
-            <img loading="lazy" src="/image/aboutUs/slideLeft/image7.jpg" className="max-w-none rounded-xl h-60 w-auto" alt="explorem" />
-            <img loading="lazy" src="/image/aboutUs/slideLeft/image8.jpg" className="max-w-none rounded-xl h-60 w-auto" alt="explorem" />
+            <img loading="lazy" src="/image/aboutUS/slideLeft/image1.jpg" className="max-w-none rounded-xl h-60 w-auto" alt="explorem" />
+            <img loading="lazy" src="/image/aboutUS/slideLeft/image2.jpg" className="max-w-none rounded-xl h-60 w-auto" alt="explorem" />
+            <img loading="lazy" src="/image/aboutUS/slideLeft/image3.jpg" className="max-w-none rounded-xl h-60 w-auto" alt="explorem" />
+            <img loading="lazy" src="/image/aboutUS/slideLeft/image4.jpg" className="max-w-none rounded-xl h-60 w-auto" alt="explorem" />
+            <img loading="lazy" src="/image/aboutUS/slideLeft/image5.jpg" className="max-w-none rounded-xl h-60 w-auto" alt="explorem" />
+            <img loading="lazy" src="/image/aboutUS/slideLeft/image6.jpg" className="max-w-none rounded-xl h-60 w-auto" alt="explorem" />
+            <img loading="lazy" src="/image/aboutUS/slideLeft/image7.jpg" className="max-w-none rounded-xl h-60 w-auto" alt="explorem" />
+            <img loading="lazy" src="/image/aboutUS/slideLeft/image8.jpg" className="max-w-none rounded-xl h-60 w-auto" alt="explorem" />
         
 
         </div>
         <div className="flex space-x-16 animate-loop-scroll " aria-hidden="true">
-        <img loading="lazy" src="/image/aboutUs/slideLeft/image1.jpg" className="max-w-none rounded-xl h-60 w-auto" alt="explorem" />
-            <img loading="lazy" src="/image/aboutUs/slideLeft/image2.jpg" className="max-w-none rounded-xl h-60 w-auto" alt="explorem" />
-            <img loading="lazy" src="/image/aboutUs/slideLeft/image3.jpg" className="max-w-none rounded-xl h-60 w-auto" alt="explorem" />
-            <img loading="lazy" src="/image/aboutUs/slideLeft/image4.jpg" className="max-w-none rounded-xl h-60 w-auto" alt="explorem" />
-            <img loading="lazy" src="/image/aboutUs/slideLeft/image5.jpg" className="max-w-none rounded-xl h-60 w-auto" alt="explorem" />
-            <img loading="lazy" src="/image/aboutUs/slideLeft/image6.jpg" className="max-w-none rounded-xl h-60 w-auto" alt="explorem" />
-            <img loading="lazy" src="/image/aboutUs/slideLeft/image7.jpg" className="max-w-none rounded-xl h-60 w-auto" alt="explorem" />
-            <img loading="lazy" src="/image/aboutUs/slideLeft/image8.jpg" className="max-w-none rounded-xl h-60 w-auto" alt="explorem" />
+        <img loading="lazy" src="/image/aboutUS/slideLeft/image1.jpg" className="max-w-none rounded-xl h-60 w-auto" alt="explorem" />
+            <img loading="lazy" src="/image/aboutUS/slideLeft/image2.jpg" className="max-w-none rounded-xl h-60 w-auto" alt="explorem" />
+            <img loading="lazy" src="/image/aboutUS/slideLeft/image3.jpg" className="max-w-none rounded-xl h-60 w-auto" alt="explorem" />
+            <img loading="lazy" src="/image/aboutUS/slideLeft/image4.jpg" className="max-w-none rounded-xl h-60 w-auto" alt="explorem" />
+            <img loading="lazy" src="/image/aboutUS/slideLeft/image5.jpg" className="max-w-none rounded-xl h-60 w-auto" alt="explorem" />
+            <img loading="lazy" src="/image/aboutUS/slideLeft/image6.jpg" className="max-w-none rounded-xl h-60 w-auto" alt="explorem" />
+            <img loading="lazy" src="/image/aboutUS/slideLeft/image7.jpg" className="max-w-none rounded-xl h-60 w-auto" alt="explorem" />
+            <img loading="lazy" src="/image/aboutUS/slideLeft/image8.jpg" className="max-w-none rounded-xl h-60 w-auto" alt="explorem" />
            
         </div>
     </div>
