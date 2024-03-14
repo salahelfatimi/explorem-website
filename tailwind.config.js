44c959759cece19fcc719aca155c069cc973f782
salahelfatimi <salahfatimi16@gmail.com>
2024-03-14 18:44:26 +0000

    add scrool with javascript

--- tailwind.config.js
+++ tailwind.config.js
@@ -33,15 +33,7 @@ module.exports = {
       },
     },
     extend: {
-      animation: {
-        'loop-scroll': 'loop-scroll 25s linear infinite',
-      },
-      keyframes: {
-        'loop-scroll': {
-          from: { transform: 'translateX(0)' },
-          to: { transform: 'translateX(-100%)' },
-        }
-      }    
+       
     },
   },
   plugins: [],
