bca4b36cf28808a69b41596d927ff944b427acac
salahelfatimi <salahfatimi16@gmail.com>
2024-03-15 14:03:47 +0000

    add

--- src/pages/components/infiniteHorizontalScroll.jsx
+++ src/pages/components/infiniteHorizontalScroll.jsx
@@ -4,7 +4,7 @@ const InfiniteHorizontalScroll = ({images}) => {
     <div className="flex overflow-hidden space-x-16 group ">
         <div className="flex space-x-16 animate-loop-scroll ">
             {
-                images.map((image,index)=>(
+                images?.map((image,index)=>(
                     <div key={index}>
                         <img  src={`/image/aboutUS/slideLeft/image${image}.jpg`} className="max-w-none rounded-xl h-60 w-auto" alt="explorem" />
                     </div>
@@ -13,7 +13,7 @@ const InfiniteHorizontalScroll = ({images}) => {
         </div>
         <div className="flex space-x-16 lg:animate-loop-scroll " aria-hidden="true">
             {
-                images.map((image,index)=>(
+                images?.map((image,index)=>(
                     <div key={index}>
                         <img  src={`/image/aboutUS/slideLeft/image${image}.jpg`} className="max-w-none rounded-xl h-60 w-auto" alt="explorem" />
                     </div>
