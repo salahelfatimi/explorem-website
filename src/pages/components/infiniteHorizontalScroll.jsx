dc000cde51ce8a4a4b871d33dca770b33b7e3821
salahelfatimi <salahfatimi16@gmail.com>
2024-03-15 12:36:08 +0000

    add

--- src/pages/components/infiniteHorizontalScroll.jsx
+++ src/pages/components/infiniteHorizontalScroll.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const InfiniteHorizontalScroll = ({ images }) => {
   return (
     <div className="flex overflow-hidden space-x-16 group ">
-      <div className="flex space-x-16 loop-scroll animate-loop-scroll">
+      <div className="flex space-x-16 loop-scroll lg:animate-loop-scroll">
         {images?.map((image, index) => (
           <div key={index}>
             <img src={image} className="max-w-none rounded-xl h-60 w-auto" alt="explorem" />
