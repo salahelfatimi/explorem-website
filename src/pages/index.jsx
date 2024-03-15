322cba771116f4797195ba36cb05012e44c3e5c6
salahelfatimi <salahfatimi16@gmail.com>
2024-03-15 12:39:40 +0000

    qa

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,7 @@
 "use client"
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { useTranslation } from 'next-i18next'
+import InfiniteHorizontalScroll from './components/infiniteHorizontalScroll'
 export async function getStaticProps({ locale }) {
   return {
     props: {
@@ -14,9 +15,20 @@ export async function getStaticProps({ locale }) {
 }
 export default function Index(){
     const { t } = useTranslation('index')
+    const images1 = [
+      '/image/aboutUS/slideLeft/image1.jpg',
+      '/image/aboutUS/slideLeft/image2.jpg',
+      '/image/aboutUS/slideLeft/image3.jpg',
+      '/image/aboutUS/slideLeft/image4.jpg',
+      '/image/aboutUS/slideLeft/image5.jpg',
+      '/image/aboutUS/slideLeft/image6.jpg',
+      
+    ];
     return(
         <>
             <span>{t('index')}</span>
+            <InfiniteHorizontalScroll images={images1} />
+
         </>
     )
 }
