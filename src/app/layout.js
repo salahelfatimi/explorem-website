f23a97335009809fbd0be63541ad47a0546105be
salahelfatimi <salahfatimi16@gmail.com>
2024-03-20 20:45:37 +0000

    add Analytics and SpeedInsights

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,16 @@
-import './globals.css'
-
-
-
-
+import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react"
 
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">     
-      <body >{children}</body>
+    <html lang="en">
+      <body>
+        {children}
+        <SpeedInsights />
+        <Analytics />
+      </body>
     </html>
-  )
+  );
 }
