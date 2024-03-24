ceee3fc0565113a214c1fc9dfa0e92ab4476caff
salahelfatimi <salahfatimi16@gmail.com>
2024-03-24 15:11:45 +0000

    add

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
-
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 
 
@@ -8,7 +9,8 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body>
         {children}
-        
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
