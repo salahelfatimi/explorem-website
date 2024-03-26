777eee13168552230fb431ae793ef7e0b6aa609c
salahelfatimi <salahfatimi16@gmail.com>
2024-03-26 20:53:52 +0000

    add

--- src/pages/components/infiniteHorizontalScroll.jsx
+++ src/pages/components/infiniteHorizontalScroll.jsx
@@ -1,57 +1,35 @@
 "use client";
 
 import Image from "next/image";
+import { Swiper, SwiperSlide } from "swiper/react";
+import {
+  Autoplay,
+  Controller,
+  FreeMode,
+  Grid,
+  Mousewheel,
+} from "swiper/modules";
 
-import React from "react";
-import { motion } from "framer-motion";
-
-const slides = [
-  { number: 1 },
-  { number: 2 },
-  { number: 3 },
-  { number: 4 },
-  { number: 5 },
-];
-
+import "swiper/css";
 const InfiniteHorizontalScroll = ({ images }) => {
-  // Duplicate the slides array to ensure seamless looping
-  const duplicatedImages = [...images, ...images];
-
   return (
-    <div className="relative w-full overflow-hidden">
-      {/* Wrapping div for seamless looping */}
-      <motion.div
-        className="flex"
-        animate={{
-          x: ["-100%", "0%"],
-          transition: {
-            ease: "linear",
-            duration: 25,
-            repeat: Infinity,
-          },
-        }}
-      >
-        {/* Render duplicated slides */}
-        {duplicatedImages.map((slide, index) => (
-          <div
-            key={index}
-            className="flex-shrink-0 "
-            style={{ width: `${100 / slides.length}%` }}
-          >
-            <div className="flex flex-col gap-4 items-center justify-center h-full text-6xl">
-              <Image
-                width={1200}
-                priority={false}
-                height={550}
-                src={slide.src}
-                className=" rounded-sm h-64 object-cover "
-                alt="explorem"
-              />
-            </div>
-          </div>
-        ))}
-      </motion.div>
-    </div>
+    <div class="relative flex overflow-x-hidden">
+  <div class="py-12 animate-marquee whitespace-nowrap">
+    <span class="mx-4 text-4xl">Marquee Item 1</span>
+    <span class="mx-4 text-4xl">Marquee Item 2</span>
+    <span class="mx-4 text-4xl">Marquee Item 3</span>
+    <span class="mx-4 text-4xl">Marquee Item 4</span>
+    <span class="mx-4 text-4xl">Marquee Item 5</span>
+  </div>
+
+  <div class="absolute top-0 py-12 animate-marquee2 whitespace-nowrap">
+    <span class="mx-4 text-4xl">Marquee Item 1</span>
+    <span class="mx-4 text-4xl">Marquee Item 2</span>
+    <span class="mx-4 text-4xl">Marquee Item 3</span>
+    <span class="mx-4 text-4xl">Marquee Item 4</span>
+    <span class="mx-4 text-4xl">Marquee Item 5</span>
+  </div>
+</div>
   );
 };
 
